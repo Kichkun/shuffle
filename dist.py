504e8102dbae86a9dc4d80310f96052968ddc5ed
Anastasia Kishkun <Kichkun@yandex.ru>
2019-10-10 20:15:59 +0300

    preprocess

---
+++ dist.py
@@ -0,0 +1,80 @@
+import numpy as np
+import os
+import skimage
+#from joblib import Parallel, delayed
+from datetime import datetime
+from numba import jit
+from multiprocessing import Process
+
+def dist(path, data_path, p, prefix):
+    print('started', prefix)
+    start = datetime.now()
+    m = 512 // p
+    files = sorted(os.listdir(path))
+    images = []
+    for file in files:
+        full_path = os.path.join(path, file)
+        image = skimage.io.imread(full_path)
+        images.append(image)
+    left = np.zeros((len(images), m, m, p, 3), dtype=np.int32)
+    right = np.zeros((len(images), m, m, p, 3), dtype=np.int32)
+    up = np.zeros((len(images), m, m, p, 3), dtype=np.int32)
+    down = np.zeros((len(images), m, m, p, 3), dtype=np.int32)
+    for i in range(len(images)):
+        image = images[i]
+        if len(image.shape) == 2:
+            image = np.stack((image // 256,)*3, axis=-1)
+        for x in range(m):
+            for y in range(m):
+                up[i][x][y] = image[p * x, p * y:p * (y + 1)]
+                down[i][x][y] = image[p * x + p - 1, p * y:p * (y + 1)]
+                left[i][x][y] = image[p * x: p * (x + 1), p * y]
+                right[i][x][y] = image[p * x: p * (x + 1), p * y + p - 1]
+    ud = np.zeros((len(images), m, m, m, m), dtype=np.int32)
+    lr = np.zeros((len(images), m, m, m, m), dtype=np.int32)
+    compute(m, p, left, right, up, down, len(images), ud, lr)
+    np.save(os.path.join(data_path, 'ud' + prefix), ud)
+    np.save(os.path.join(data_path, 'lr' + prefix), lr)
+    print(prefix, datetime.now() - start)
+
+@jit(nopython=True)
+def compute(m, p, left, right, up, down, image_len, ud, lr):
+    for i in range(image_len):
+        #if i % 50 == 0:
+        #    print(i / image_len * 100)
+        for x1 in range(m):
+            for y1 in range(m):
+                for x2 in range(m):
+                    for y2 in range(m):
+                        ud[i][x1][y1][x2][y2] = np.square(down[i][x1][y1] - up[i][x2][y2]).sum()
+                        lr[i][x1][y1][x2][y2] = np.square(right[i][x1][y1] - left[i][x2][y2]).sum()
+
+
+arg1 = ['/home/ksihkun/git_projects/shuffle/data_train/64-sources',\
+       '/home/ksihkun/git_projects/shuffle/data_train/64',\
+       '/home/ksihkun/git_projects/shuffle/data_train/32-sources',\
+       '/home/ksihkun/git_projects/shuffle/data_train/32',\
+       '/home/ksihkun/git_projects/shuffle/data_train/16-sources',\
+       '/home/ksihkun/git_projects/shuffle/data_train/16',\
+       '/home/ksihkun/git_projects/shuffle/data_test1_blank/64',\
+       '/home/ksihkun/git_projects/shuffle/data_test1_blank/32',\
+       '/home/ksihkun/git_projects/shuffle/data_test1_blank/16',\
+       '/home/ksihkun/git_projects/shuffle/data_test2_blank/64',\
+       '/home/ksihkun/git_projects/shuffle/data_test2_blank/32',\
+       '/home/ksihkun/git_projects/shuffle/data_test2_blank/16']
+
+arg2 = '/home/ksihkun/git_projects/shuffle/data_train/data_sqr'
+arg3 = [64, 64, 32, 32, 16, 16, 64, 32, 16, 64, 32, 16]
+arg4 = ['train-64-sources', 'train-64', 'train-32-sources', 'train-32', 'train-16-sources', 'train-16',\
+        'test1-64', 'test1-32', 'test1-16', 'test2-64', 'test2-32', 'test2-16']
+#Parallel(n_jobs=10, prefer="threads")(delayed(dist)(arg1[i], arg2, arg3[i], arg4[i]) for i in range(len(arg3)))
+#for i in range(len(arg3)):
+#    dist(arg1[i], arg2, arg3[i], arg4[i])
+
+if __name__ == '__main__':
+    p = []
+    for i in range(len(arg3)):
+        p.append(Process(target=dist, args=(arg1[i], arg2, arg3[i], arg4[i])))
+        p[-1].start()
+    for i in range(len(p)):
+        p[i].join()
